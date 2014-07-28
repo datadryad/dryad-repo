8aaa51f9b653776a54dc215acc58a013dc3b016d
daisieh_local <daisieh@gmail.com>
2014-07-28 10:16:33 -0700

    augh, these were supposed to be this, not parent.

--- dspace/modules/api/src/test/java/org/dspace/identifier/DOIIdentifierProviderTest.java
+++ dspace/modules/api/src/test/java/org/dspace/identifier/DOIIdentifierProviderTest.java
@@ -19,7 +19,6 @@ public class DOIIdentifierProviderTest extends DOIIdentifierProvider{
 
     @Test
     public void testDOIs() {
-        DOIIdentifierProvider dip = new DOIIdentifierProvider();
         System.out.println("Testing validity of DOIs ");
         String doi = "";
         String canonicalID = "";
@@ -31,27 +30,27 @@ public class DOIIdentifierProviderTest extends DOIIdentifierProvider{
             myLocalPartTestSuffix = "FK2.5061.dryad.";
         }
         doi = "doi:10.5061/dryad.64274";
-        canonicalID = dip.getCanonicalDataPackage(doi);
+        canonicalID = this.getCanonicalDataPackage(doi);
         System.out.println("DOI "+doi+ " has a canonical ID of "+ canonicalID);
         assertTrue("Regular DOI " + doi + " canonical ID incorrect: " + canonicalID, canonicalID.equals("doi:10.5061/dryad.64274"));
 
         doi = "doi:10.5072/FK2.5061.dryad.dn470";
-        canonicalID = dip.getCanonicalDataPackage(doi);
+        canonicalID = this.getCanonicalDataPackage(doi);
         System.out.println("DOI "+doi+ " has a canonical ID of "+ canonicalID);
         assertTrue("Test DOI " + doi + " canonical ID incorrect: " + canonicalID, canonicalID.equals("doi:10.5072/FK2.5061.dryad.dn470"));
 
         doi = "info:doi/10.5061/dryad.64274/1";
-        canonicalID = dip.getCanonicalDataPackage(doi);
+        canonicalID = this.getCanonicalDataPackage(doi);
         System.out.println("DOI "+doi+ " has a canonical ID of "+ canonicalID);
         assertTrue("File DOI " + doi + " canonical ID incorrect: " + canonicalID, canonicalID.equals("info:doi/10.5061/dryad.64274"));
 
         doi = "doi:10.5061/dryad.64274.2";
-        canonicalID = dip.getCanonicalDataPackage(doi);
+        canonicalID = this.getCanonicalDataPackage(doi);
         System.out.println("DOI "+doi+ " has a canonical ID of "+ canonicalID);
         assertTrue("Version DOI " + doi + " canonical ID incorrect: " + canonicalID, canonicalID.equals("doi:10.5061/dryad.64274"));
 
         doi = "doi:10.5061/dryad.64274.2/2.2";
-        canonicalID = dip.getCanonicalDataPackage(doi);
+        canonicalID = this.getCanonicalDataPackage(doi);
         System.out.println("DOI "+doi+ " has a canonical ID of "+ canonicalID);
         assertTrue("Versioned file DOI " + doi + " canonical ID incorrect: " + canonicalID, canonicalID.equals("doi:10.5061/dryad.64274"));
 
