86d8d368f8c4e8b785be050bc8e9f063ec1e5bc3
daisie_local <daisieh@gmail.com>
2015-07-23 00:05:03 -0700

    test which file

--- test/bin/travis-install-dryad-dir.sh
+++ test/bin/travis-install-dryad-dir.sh
@@ -2,10 +2,5 @@
 
 sudo mkdir -p -m 0755 /opt/dryad-test
 sudo chown -R $USER /opt/dryad-test
-echo "copy from: "
-ls -la $TRAVIS_BUILD_DIR/test/config
-echo "copy to: "
-ls -la /opt/dryad-test/config
-echo "copying now"
 cp -L -r $TRAVIS_BUILD_DIR/test/config /opt/dryad-test/config
-ls -la /opt/dryad-test/config
+grep "log.init.config" /opt/dryad-test/config/*
