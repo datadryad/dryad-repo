698ecd9f3dd7bc4c86a91858f96977a8de0f5108
daisie_local <daisieh@gmail.com>
2015-07-22 23:49:59 -0700

    look at the dirs

--- test/bin/travis-install-dryad-dir.sh
+++ test/bin/travis-install-dryad-dir.sh
@@ -2,4 +2,6 @@
 
 sudo mkdir -p -m 0755 /opt/dryad-test
 sudo chown -R $USER /opt/dryad-test
+ls -la $TRAVIS_BUILD_DIR/test/config
 cp -L -r $TRAVIS_BUILD_DIR/test/config /opt/dryad-test/config
+ls -la /opt/dryad-test/config
