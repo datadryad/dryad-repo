cddae959f7ff40d9515b51b1be19acded8a31acc
daisie_local <daisieh@gmail.com>
2015-07-24 11:42:04 -0700

    edit permissions?

--- test/bin/travis-install-dryad-dir.sh
+++ test/bin/travis-install-dryad-dir.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo mkdir -p -m 0755 /opt/dryad-test
-sudo chown -R $USER /opt/dryad-test
-cp -L -r $TRAVIS_BUILD_DIR/test/config /opt/dryad-test/config
-cp -r $TRAVIS_BUILD_DIR/dspace-api/src/test/resources/dspaceFolder/testData /opt/dryad-test/
+cp -L -R $TRAVIS_BUILD_DIR/test/config /opt/dryad-test/config
+cp -R $TRAVIS_BUILD_DIR/dspace-api/src/test/resources/dspaceFolder/testData /opt/dryad-test/
+sudo chmod -R 0755 /opt/dryad-test
 ls -l /opt/dryad-test/config
