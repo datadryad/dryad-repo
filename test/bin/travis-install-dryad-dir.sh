765a1f17acd351a13b90e0708c3238deefaa8bbb
daisie_local <daisieh@gmail.com>
2015-07-24 14:16:58 -0700

    use standard paths

--- test/bin/travis-install-dryad-dir.sh
+++ test/bin/travis-install-dryad-dir.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo mkdir -p -m 0755 /opt/dryad-test
-sudo chown -R $USER /opt/dryad-test
-cp -L -r $TRAVIS_BUILD_DIR/test/config /opt/dryad-test/config
+sudo mkdir -p -m 0755 /opt/dryad
+sudo chown -R $USER /opt/dryad
+cp -L -r $TRAVIS_BUILD_DIR/test/config /opt/dryad/config
 cp $TRAVIS_BUILD_DIR/dspace/etc/server/settings.xml $HOME/.m2/
