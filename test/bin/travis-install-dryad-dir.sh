c6c73cdc09c1ed42d6373b913dca8e4c429ddcf2
daisie_local <daisieh@gmail.com>
2015-07-24 15:08:18 -0700

    test

--- test/bin/travis-install-dryad-dir.sh
+++ test/bin/travis-install-dryad-dir.sh
@@ -2,8 +2,9 @@
 
 sudo mkdir -p -m 0755 /opt/dryad
 sudo mkdir -p -m 0755 /opt/dryad/test
+sudo mkdir -p -m 0755 /opt/dryad/test/testData
 sudo chown -R $USER /opt/dryad
 cp -L -r $TRAVIS_BUILD_DIR/test/config /opt/dryad/config
-cp -R $TRAVIS_BUILD_DIR/dspace-api/src/test/resources/dspaceFolder /opt/dryad/test/
+cp -R $TRAVIS_BUILD_DIR/dspace-api/src/test/resources/dspaceFolder/testData /opt/dryad/test/testData
 cp $TRAVIS_BUILD_DIR/dspace/etc/server/settings.xml $HOME/.m2/
 ls -lR /opt/dryad/test
