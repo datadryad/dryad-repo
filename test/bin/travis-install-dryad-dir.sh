15b32dfe4b8722c1df5bedcc0a29d233592d1783
daisie_local <daisieh@gmail.com>
2015-07-24 11:58:02 -0700

    777

--- test/bin/travis-install-dryad-dir.sh
+++ test/bin/travis-install-dryad-dir.sh
@@ -3,5 +3,5 @@
 sudo mkdir -p -m 0755 /opt/dryad-test
 cp -L -R $TRAVIS_BUILD_DIR/test/config /opt/dryad-test/config
 cp -R $TRAVIS_BUILD_DIR/dspace-api/src/test/resources/dspaceFolder/testData /opt/dryad-test/
-sudo chmod -R 0755 /opt/dryad-test
+sudo chmod -R 0777 /opt/dryad-test
 ls -l /opt/dryad-test/config
