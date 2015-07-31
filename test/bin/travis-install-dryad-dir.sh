5c7dbf04484e777102d49f34b709bc164a0c5afb
daisie_local <daisieh@gmail.com>
2015-07-31 16:47:21 -0700

    move lines around

--- test/bin/travis-install-dryad-dir.sh
+++ test/bin/travis-install-dryad-dir.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
-sudo mkdir -p -m 0755 /opt/dryad
-sudo mkdir -p -m 0755 /opt/dryad/test
+export DRYAD_CODE_DIR=.
+# Copy test directory to local filesystem, outside of source repo, and owned by dryad user
+export DRYAD_TEST_DIR="/opt/dryad/test"
+
+
+sudo mkdir -p -m 0755 $DRYAD_TEST_DIR
 sudo mkdir -p -m 0755 /opt/dryad/test/testData
 sudo mkdir -p -m 0755 /opt/dryad/test/assetstore
 sudo chown -R $USER /opt/dryad
-cp -L -r $TRAVIS_BUILD_DIR/test/config /opt/dryad/config
-cp -R $TRAVIS_BUILD_DIR/dspace-api/src/test/resources/dspaceFolder/testData/* /opt/dryad/test/testData/
+cp -L -r $TRAVIS_BUILD_DIR/test/config $DRYAD_TEST_DIR/config
+cp -R $TRAVIS_BUILD_DIR/dspace-api/src/test/resources/dspaceFolder/testData/* $DRYAD_TEST_DIR/testData/
 cp $TRAVIS_BUILD_DIR/dspace/etc/server/settings.xml $HOME/.m2/
-ls -lR /opt/dryad/test
+
+# These are needed somehow by bagit ingest tests
+cp -p -r "${DRYAD_CODE_DIR}/dspace/config/crosswalks" "${DRYAD_TEST_DIR}/config/"
+cp -p -r "${DRYAD_CODE_DIR}/dspace/config/dspace-solr-search.cfg" "${DRYAD_TEST_DIR}/config/"
+
+# Update the testing version of dspace.cfg with database credentials
+# Ansible provisioning installs password into .pgpass, so fish it out of there
+
+cat "${DRYAD_CODE_DIR}/dspace/config/dspace.cfg" \
+  | sed "s|db.username =.*|db.username = ${PGUSER}|g" \
+  | sed "s|dspace.dir = .*|dspace.dir = ${DRYAD_TEST_DIR}|g" \
+  | sed "s|doi.service.testmode= .*|doi.service.testmode= true|g" \
+  | sed "s|doi.datacite.connected = .*|doi.datacite.connected = false|g" \
+  | sed "s|dspace.url = .*|dspace.url = http://localhost:9999|g" \
+  >"${DRYAD_TEST_DIR}/config/dspace.cfg"
--- test/bin/travis-install-test-database.sh
+++ test/bin/travis-install-test-database.sh
@@ -7,7 +7,6 @@
 export PGUSER=dryad_test_user
 export PGDATABASE=dryad_test_db
 
-DRYAD_CODE_DIR=.
 
 # Drop the dryad_test database
 # dropdb/createdb require PGPASSWORD
@@ -27,22 +26,3 @@ psql < ${DRYAD_CODE_DIR}/test/etc/postgres/test-system-curator.sql
 psql < ${DRYAD_CODE_DIR}/dspace/etc/postgres/update-sequences.sql
 psql < ${DRYAD_CODE_DIR}/test/etc/postgres/test-journal-landing.sql
 
-# Copy test directory to local filesystem, outside of source repo, and owned by dryad user
-DRYAD_TEST_DIR="/opt/dryad/test"
-
-cp -L -p -r "${DRYAD_CODE_DIR}/test" "${DRYAD_TEST_DIR}"
-mkdir ${DRYAD_TEST_DIR}/config
-# These are needed somehow by bagit ingest tests
-cp -p -r "${DRYAD_CODE_DIR}/dspace/config/crosswalks" "${DRYAD_TEST_DIR}/config/"
-cp -p -r "${DRYAD_CODE_DIR}/dspace/config/dspace-solr-search.cfg" "${DRYAD_TEST_DIR}/config/"
-
-# Update the testing version of dspace.cfg with database credentials
-# Ansible provisioning installs password into .pgpass, so fish it out of there
-
-cat "${DRYAD_CODE_DIR}/dspace/config/dspace.cfg" \
-  | sed "s|db.username =.*|db.username = ${PGUSER}|g" \
-  | sed "s|dspace.dir = .*|dspace.dir = ${DRYAD_TEST_DIR}|g" \
-  | sed "s|doi.service.testmode= .*|doi.service.testmode= true|g" \
-  | sed "s|doi.datacite.connected = .*|doi.datacite.connected = false|g" \
-  | sed "s|dspace.url = .*|dspace.url = http://localhost:9999|g" \
-  >"${DRYAD_TEST_DIR}/config/dspace.cfg"
