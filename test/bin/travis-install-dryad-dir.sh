16699b5dd951559df0ea3d462b537f4b9b85104a
daisie_local <daisieh@gmail.com>
2015-08-01 01:36:07 -0700

    cleaning up scripts

--- test/bin/travis-install-dryad-dir.sh
+++ test/bin/travis-install-dryad-dir.sh
@@ -3,7 +3,8 @@
 export DRYAD_CODE_DIR=.
 # Copy test directory to local filesystem, outside of source repo, and owned by dryad user
 export DRYAD_TEST_DIR="/opt/dryad/test"
-
+export PGUSER=dryad_test_user
+export PGDATABASE=dryad_test_db
 
 sudo mkdir -p -m 0755 $DRYAD_TEST_DIR
 sudo mkdir -p -m 0755 /opt/dryad/test/testData
@@ -30,3 +31,23 @@ cat "${DRYAD_CODE_DIR}/dspace/config/dspace.cfg" \
   | sed "s|db.username = .*|db.username = dryad_test_user|g" \
   | sed "s|db.password = .*|db.password =|g" \
   >"${DRYAD_TEST_DIR}/config/dspace.cfg"
+
+psql -U postgres -c "create user dryad_app with createdb;" -d template1
+createdb -U dryad_app dryad_repo
+psql -U postgres -c "create user dryad_test_user with createdb;" -d template1
+createdb -U dryad_test_user dryad_test_db
+
+# Load database schema
+psql < ${DRYAD_CODE_DIR}/dspace/etc/postgres/database_schema.sql
+psql < ${DRYAD_CODE_DIR}/dspace/etc/postgres/database_create_doi_table.sql
+psql < ${DRYAD_CODE_DIR}/dspace/etc/postgres/database_change_payment_system.sql
+psql < ${DRYAD_CODE_DIR}/dspace/etc/postgres/database_terms_and_condition.sql
+psql < ${DRYAD_CODE_DIR}/dspace/etc/collection-workflow-changes.sql
+psql < ${DRYAD_CODE_DIR}/dspace/etc/atmire-versioning-changes.sql
+psql < ${DRYAD_CODE_DIR}/dspace/etc/postgres/database_dryad_groups_collections.sql
+psql < ${DRYAD_CODE_DIR}/dspace/etc/postgres/database_dryad_registries.sql
+psql < ${DRYAD_CODE_DIR}/dspace/etc/postgres/dryad-rest-webapp.sql
+psql < ${DRYAD_CODE_DIR}/test/etc/postgres/test-system-curator.sql
+psql < ${DRYAD_CODE_DIR}/dspace/etc/postgres/update-sequences.sql
+psql < ${DRYAD_CODE_DIR}/test/etc/postgres/test-journal-landing.sql
+
