96b824989bc543a5da5b91b3a33e48263bd03f57
daisie_local <daisieh@gmail.com>
2016-10-22 20:52:43 -0700

    journals resource path is always authorized

--- dspace/modules/api/src/main/java/org/datadryad/rest/auth/AuthenticationRequestFilter.java
+++ dspace/modules/api/src/main/java/org/datadryad/rest/auth/AuthenticationRequestFilter.java
@@ -10,7 +10,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response.Status;
 
 import org.apache.log4j.Logger;
-import org.apache.oltu.oauth2.common.error.OAuthError;
 import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.common.message.types.ParameterStyle;
@@ -29,23 +28,25 @@ public class AuthenticationRequestFilter implements ContainerRequestFilter {
     public ContainerRequest filter(ContainerRequest containerRequest) {
         log.info("Filtering request for authentication");
         // TODO: Make sure HTTPs if we can
+        OAuthAccessResourceRequest oAuthRequest = null;
+        String accessToken = null;
         try {
             // This allows us to get tokens out of query parameter or header
-            OAuthAccessResourceRequest oAuthRequest = new OAuthAccessResourceRequest(servletRequest, ParameterStyle.QUERY, ParameterStyle.HEADER);
-            String accessToken = oAuthRequest.getAccessToken();
-            EPersonUserPrincipal userPrincipal = authHelper.getPrincipalFromToken(accessToken);
-            if(userPrincipal == null) {
-                AuthHelper.throwExceptionResponse(null, Status.UNAUTHORIZED, OAuthError.ResourceResponse.INVALID_TOKEN);
-            } else {
-                // User found, set it into the context.
-                EPersonSecurityContext securityContext = new EPersonSecurityContext(userPrincipal);
-                containerRequest.setSecurityContext(securityContext);
-                return containerRequest;
-            }
+            oAuthRequest = new OAuthAccessResourceRequest(servletRequest, ParameterStyle.QUERY, ParameterStyle.HEADER);
+            accessToken = oAuthRequest.getAccessToken();
         } catch (OAuthProblemException ex) {
-            AuthHelper.throwExceptionResponse(ex, Status.INTERNAL_SERVER_ERROR, "OAuth Problem");
+            log.error("OAuth problem");
         } catch (OAuthSystemException ex) {
             AuthHelper.throwExceptionResponse(ex, Status.INTERNAL_SERVER_ERROR, "OAuth System Exception");
+        } catch (NullPointerException e) {
+            // This can legitimately happen if an access token isn't provided in either the query or the header
+            log.error("No OAuth in query or header");
+        }
+        EPersonUserPrincipal userPrincipal = authHelper.getPrincipalFromToken(accessToken);
+        if(userPrincipal != null) {
+            // User found, set it into the context.
+            EPersonSecurityContext securityContext = new EPersonSecurityContext(userPrincipal);
+            containerRequest.setSecurityContext(securityContext);
         }
         return containerRequest;
     }
